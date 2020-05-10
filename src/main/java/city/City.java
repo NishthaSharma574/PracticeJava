caf410de6df4b99489d5947e7b768de08e5e5182
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-09 23:13:19 -0700

    City Added

---
+++ src/main/java/city/City.java
@@ -0,0 +1,10 @@
+package city;
+
+public class City {
+    public void add()
+    {
+        System.out.println("Pathankot");
+        System.out.println("Amritsar");
+        System.out.println("Jaipur");
+    }
+}
