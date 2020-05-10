7a1f2bcb55c898b3c7496f9f862bd3598d82093a
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-09 23:09:18 -0700

    States Added

---
+++ src/main/java/states/States.java
@@ -0,0 +1,10 @@
+package states;
+
+public class States {
+    public void add()
+    {
+        System.out.println("Punjab");
+        System.out.println("Rajasthan");
+        System.out.println("Andhra Pradesh");
+    }
+}
