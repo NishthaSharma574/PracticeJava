043ecf37d6302587dda7153f6b630eac7c2b9b3b
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-09 22:28:14 -0700

    First Commit

---
+++ src/main/java/country/Country.java
@@ -0,0 +1,9 @@
+package country;
+
+public class Country {
+    public static void main(String[] args) {
+        System.out.println("India");
+        System.out.println("America");
+        System.out.println("Japan");
+    }
+}
